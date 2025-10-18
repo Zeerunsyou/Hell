1c65328fb7e5c36848df7805c8240b3439b83804
Zeerunsyou <155188630+Zeerunsyou@users.noreply.github.com>
2025-10-18 19:44:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const commands = [
   { name: "autoreact add", category: "Moderation", description: "Add Autoreactions for users.", usage: "`autoreact add <userId> <emoji>" },
   { name: "autoreact remove", category: "Moderation", description: "Remove Autoreactions for users.", usage: "`autoreact remove <userId> <emoji>" },
   { name: "autoreactlist", category: "Moderation", description: "List autoreactions that users have.", usage: "`autoreactlist" },
-  { name: "autoreactlist", category: "Moderation", description: "Automatically send a embed to a channel when a member joins the server.", usage: "`autowelcome #channel" },
+  { name: "autowelcome", category: "Moderation", description: "Automatically send a embed to a channel when a member joins the server.", usage: "`autowelcome #channel" },
   { name: "banner", category: "User", description: "Displays a user's banner.", usage: "`banner @user" },
   { name: "boostnotify", category: "Moderation", description: "Automatically send a message to a channel when a member boosts the server", usage: "`boostnotify #channel" },
   { name: "calc", category: "Fun", description: "Calculate a math expression", usage: "`calc <expression>" },
@@ -89,6 +89,7 @@ searchInput.addEventListener("input", () => {
   );
   displayCommands(filtered);
 });
+
 
 
 
